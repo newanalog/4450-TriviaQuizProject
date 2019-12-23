771aa03b7c10db3e1770d362ae61d4729728a153
newanalog <790261+newanalog@users.noreply.github.com>
2019-12-23 12:44:07 -0600

    Milestone 1.2 - Validate questions (#3)
    Added ability to validate each question in the current quiz session.
    The QuizService is responsible for validating each question and
    returning an array that holds the indexes of missed questions.  This can
    be used later to mark questions in the UI rather than through an alert
    box.

--- js/QuizService.js
+++ js/QuizService.js
@@ -6,6 +6,7 @@
 */
 export default class {
     constructor() {
+        // array to hold questions for this quiz session
         this.questions = [];
     }
 
@@ -14,10 +15,28 @@ export default class {
     }
 
     getQuestions() {
+        // return all the questions for this session
         return this.questions;
+    }
+
+    getQuestionCount() {
+        return this.questions.length;
     }
 
     clearQuestions() {
         this.questions = [];
+    }
+
+    validateQuestions() {
+        const errors = [];
+        // call each question in the current quiz's validate method
+        // and push any errors to the array to be returned
+        this.questions.forEach((question, index) => {
+            if (!question.validate()) {
+                errors.push(index);
+            }
+        });
+
+        return errors;
     }
 }
