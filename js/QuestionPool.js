12a95d85e461e2a5175fa6d7585f815339a936f8
newanalog <790261+newanalog@users.noreply.github.com>
2019-12-23 18:39:54 -0600

    Fixed CORS errors by abandoning modules (#4)
    Realizing this page may be run by just opening the index.html file, I
    removed the external script references to avoid CORS errors that were
    encountered when using file:// protocol.  Script is now all inside
    index.html file. The project can be viewed by just opening index.html.

--- js/QuestionPool.js
+++
@@ -1,7 +0,0 @@
-/*
-    QuestionPool.js
-    This array is used to store the question data in an array
-*/
-export default [
-    ["multiple-choice", "What is your favorite color?", "Red", "Green", "Blue", "Red"]
-];
